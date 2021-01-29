d6cbfad4cb4ce29b4a4c4436460b0fdcf67a7e2e
Soheil Vaseghi <vaseghi.soheil@gmail.com>
2021-01-29 12:45:44 +1100

    Used writeln for adding styles

--- js/master.js
+++ js/master.js
@@ -1,6 +1,9 @@
-$.get('/shared/head.html', head => {
-  $('head').append(head);
-})
+document.writeln('<meta charset="UTF-8" />');
+document.writeln('<meta name="viewport" content="width=device-width, initial-scale=1.0" />');
+document.writeln('<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet" />');
+document.writeln('<link rel="stylesheet" href="/css/reset.css" />');
+document.writeln('<link rel="stylesheet" href="/css/styles.css" />');
+document.writeln('<link rel="stylesheet" href="/css/theme.css" />');
 
 $.get('/shared/header.html', header => {
   $('body').prepend(header);
